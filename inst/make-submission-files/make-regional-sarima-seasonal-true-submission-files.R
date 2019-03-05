0abcbdd808323aa6c89439c531a3d1ce0855069d
gcgibson <gcgibson@umass.edu>
2019-03-05 23:48:04 +0000

    blah

--- inst/make-submission-files/make-regional-sarima-seasonal-true-submission-files.R
+++ inst/make-submission-files/make-regional-sarima-seasonal-true-submission-files.R
@@ -57,7 +57,7 @@ lm_fit_hierarchical <- lmer(X0~Incidence +season_week + (1|Region), data=subset_
 fully_observed_data <- as.data.frame(readRDS("./data/fully_observed_data_formatted.rds"))
 
 
-for (analysis_time_season in c("2016/2017","2017/2018")){
+for (analysis_time_season in c("2015/2016")){
   for (delay_adjustment in delay_adjustment_list){
     if(analysis_time_season == "2017/2018"){
       end_week <- 20
